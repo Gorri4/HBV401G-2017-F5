9155656d950e9d862e09c2bc7906e9f002efbcb5
Guðmundur Orri Pálsson <gudmundurorri4@gmail.com>
2017-03-01 17:25:13 +0000

    Update Seat.java

--- FlightSearch/src/model/Seat.java
+++ FlightSearch/src/model/Seat.java
@@ -1,9 +1,27 @@
 package model;
 
 public class Seat {
-
-	public Seat() {
-		// TODO Auto-generated constructor stub
+	
+	private String SEAT_NUMBER;
+	private boolean BUISNESS_CLASS;
+	private boolean booked;	
+	
+	public Seat(String SEAT_NUMBER, boolean BUISNESS_CLASS, boolean booked) {
+		this.SEAT_NUMBER = SEAT_NUMBER;
+		this.BUISNESS_CLASS = BUISNESS_CLASS;
+		this.booked = booked;
+	}
+	
+	public String getSeatNumber(){
+		return SEAT_NUMBER;
+	}
+	
+	public boolean isBuisness(){
+		return SEAT_NUMBER;
+	}
+	
+	public boolean isBooked(){
+		return booked;
 	}
 
 }
