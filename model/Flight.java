30050769378c946b11f8461eb52f4226d914883b
bjarkivk <bjarkivk95@gmail.com>
2017-03-05 11:38:13 +0000

    Update Flight.java

--- model/Flight.java
+++ model/Flight.java
@@ -1,9 +1,51 @@
 package model;
 
-public class Flight {
+import
 
-	public Flight() {
-		// TODO Auto-generated constructor stub
+public class Flight {
+	
+	private Date departureTime;
+	private Date arrivalTime;
+	private City departureCity;
+	private City arrivalCity;
+	private double flightTime;
+	private double price;
+
+	public Flight(Date departureTime, Date arrivalTime, City departureCity, City arrivalCity, double flightTime, double price) {
+		this.departureTime=departureTime;
+		this.arrivalTime=arrivalTime;
+		this.departureCity=departureCity;
+		this.arrivalCity=arrivalCity;
+		this.flightTime=flightTime;
+		this.price=price;
+	}
+	
+	public Date getDepTime(){
+		return departureTime;
+	}
+	
+	public Date getArrTime(){
+		return arrivalTime;
+	}
+	
+	public City getDepCity(){
+		return departureCity;
+	}
+	
+	public City getArrCity(){
+		return arrivalCity;
+	}
+	
+	public double getFlightTime(){
+		return flightTime;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	public Date getLocalTime(City city, Date time){
+		//skila einhvernveginn time+city.getTimeZone()
 	}
 
 }
